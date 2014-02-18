d73511d395877cc8b5c993a5a9e4796086b5774e
Pablo Eloy de Miguel Melero <mundomele@hotmail.com>
2014-02-18 22:33:13 +0100

    Update Hola.java

--- pruebaGIT/src/pruebaGIT/Hola.java
+++ pruebaGIT/src/pruebaGIT/Hola.java
@@ -3,6 +3,6 @@ package pruebaGIT;
 public class Hola {
 	public static void main(String args[])
 	{
-		// holas, probando cambios desde remoto
+		// holas, probando cambios desde remoto, prueba editado desde github
 	}
 }
